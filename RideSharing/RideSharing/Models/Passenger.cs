f176214c551f10b3241f19e69b32f7e738047460
Grigore Bivol <36542313+GrigoreBivol@users.noreply.github.com>
2019-03-26 15:16:02 +0000

    Add files via upload

---
+++ RideSharing/RideSharing/Models/Passenger.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace RideSharing.Models
+{
+    public class Passenger
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+
+
+
+        public Passenger()
+        {
+        }
+    }
+}
